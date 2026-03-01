04a747dd6f6b96a3ca6204a1cd3a9cb0382eb2e2
vins254 <mainavincent003@gmail.com>
2026-03-01 23:57:02 +0300

    initial commit

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -33,7 +33,7 @@ export default function EditPost() {
         if (files?.[0]) {
             data.set('file', files?.[0]);
         }
-        const response = await fetch('http://localhost:4000/post', {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/post`, {
             method: 'PUT',
             body: data,
             credentials: 'include',
