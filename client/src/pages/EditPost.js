361f741a0d9d9d68d93ec4e2042edef9ff99463f
vins254 <mainavincent003@gmail.com>
2026-04-17 22:32:39 +0300

    fix: import missing Link in EditPost.js to unblock vercel build

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import Editor from "../Editor";
 import CATEGORIES from "../constants/categories";
 
